1c4fadece2e60cec7fa21af9e05741e411123c41
haripriya.gummadi <haripriya.gummadi@cgi.com>
2019-09-24 18:33:31 +0530

    First

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -8,14 +8,9 @@ public class Main
 {
     public static void main( String[] args )
     {
-        ApplicationContext context =new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie1=context.getBean("MovieA",Movie.class);
-        movie1.display();
-        Movie movie2=context.getBean("MovieB",Movie.class);
-        movie2.display();
-        System.out.println(movie1==movie2);
-        Movie movie3=context.getBean("moviebean",Movie.class);
-        movie3.display();
-
+        ApplicationContext context1 =new ClassPathXmlApplicationContext("beans.xml");
+        Movie movie1=context1.getBean("moviebean",Movie.class);
+        movie1.display1("actorbean1");
+        movie1.display2("actorbean2");
     }
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,13 +1,37 @@
 package com.stackroute.domain;
 
-public class Movie {
-    private Actor actor;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
-    public Movie(Actor actor) {
-        this.actor = actor;
+public class Movie implements BeanFactoryAware , ApplicationContextAware , BeanNameAware {
+    private Actor actor;
+    private BeanFactory beanfactory;
+    private ApplicationContext context;
+    public void display1(String beanname){
+        this.actor=beanfactory.getBean(beanname,Actor.class);
+        System.out.println(this.actor.getName()+","+this.actor.getGender()+","+this.actor.getAge());
+    }
+    public void display2(String beanname){
+        this.actor=context.getBean(beanname,Actor.class);
+        System.out.println(this.actor.getName()+","+this.actor.getGender()+","+this.actor.getAge());
     }
 
-    public void display(){
-        System.out.println(this.actor.getName()+","+this.actor.getGender()+","+this.actor.getAge());
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanfactory=beanFactory;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context=applicationContext;
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("Bean name is "+s);
     }
 }
