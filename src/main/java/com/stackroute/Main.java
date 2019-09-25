34015039d5895313243739233bf6c6cdc1104505
haripriya.gummadi <haripriya.gummadi@cgi.com>
2019-09-25 09:38:25 +0530

    First

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,6 +1,4 @@
 package com.stackroute;
-
-import com.stackroute.domain.Movie;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
