20ebb2c83d6209aae2104e7b5d4b28bf8d25ca94
haripriya.gummadi <haripriya.gummadi@cgi.com>
2019-09-25 09:36:07 +0530

    First

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -2,15 +2,14 @@ package com.stackroute;
 
 import com.stackroute.domain.Movie;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main
 {
     public static void main( String[] args )
     {
-        ApplicationContext context1 =new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie1=context1.getBean("moviebean",Movie.class);
-        movie1.display1("actorbean1");
-        movie1.display2("actorbean2");
+        AbstractApplicationContext context1 =new ClassPathXmlApplicationContext("beans.xml");
+        context1.registerShutdownHook();
     }
 }
